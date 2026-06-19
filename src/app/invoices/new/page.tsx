81d2de0f8eb398bc618388addc1883ba54dfec1b
developerthierry <developerthierry@gmail.com>
2026-06-19 20:00:14 +0000

    Connect Form

--- src/app/invoices/new/page.tsx
+++ src/app/invoices/new/page.tsx
@@ -1,15 +1,31 @@
-import { Button } from "@/components/ui/button";
+"use client";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
+import { createAction } from "@/app/actions";
+import { SyntheticEvent, useState, startTransition } from "react";
+import SubmitButton from "@/components/SubmitButton";
+import Form from 'next/form'
 
 const CreateInvoice = () => {
+  const [state, setState] = useState("ready");
+
+  const handleOnSubmit = async (event: SyntheticEvent) => {
+        if (state === "pending") {
+          event.preventDefault();
+          return
+        };
+    setState("pending");
+  };
   return (
     <main className="flex flex-col justify-center  h-full gap-6 max-w-5xl mx-12 my-12">
       <div className="flex justify-between">
         <h1 className="text-3xl font-semibold">Invoices</h1>
       </div>
-      <form className="grid gap-4">
+      <Form onSubmit={handleOnSubmit}
+        className="grid gap-4"
+        action={createAction}
+      >
         <div>
           <Label htmlFor="name" className="block font-semibold text-sm mb-2">
             Billing Name
@@ -38,9 +54,9 @@ const CreateInvoice = () => {
           <Textarea id="description" name="description"></Textarea>
         </div>
         <div>
-          <Button className="w-full font-semibold">Submit</Button>
+          <SubmitButton />
         </div>
-      </form>
+      </Form>
     </main>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { sql } from "drizzle-orm";
 import { db } from "@/db";
-export default async function Home() {
+export default function Home() {
   return (
     <main className="flex flex-col justify-center min-h-screen gap-6 text-center max-w-5xl mx-auto">
       <h1 className="text-5xl font-bold">Invoice</h1>
