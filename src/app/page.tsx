26548740468855d4cc89915b7291e66704326f18
developerthierry <developerthierry@gmail.com>
2026-06-19 10:22:09 +0000

    Connect Database Xata and Drizzle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-export default function Home() {
+import { sql } from "drizzle-orm";
+import { db } from "@/db";
+export default async function Home() {
   return (
     <main className="flex flex-col justify-center min-h-screen gap-6 text-center max-w-5xl mx-auto">
       <h1 className="text-5xl font-bold">Invoice</h1>
       <p>
         <Button asChild>
-        <Link href="/dashboard">Sign Up</Link>
+          <Link href="/dashboard">Sign Up</Link>
         </Button>
       </p>
     </main>
---
+++ src/db/schema.ts
@@ -0,0 +1,11 @@
+import { PgTable, serial, timestamp , integer, text, pgEnum, pgTable} from "drizzle-orm/pg-core";
+
+export const statusEnum = pgEnum('status', ['open', 'paid', 'void', 'uncollectible'])
+
+export const Invoices = pgTable('invoices', {
+    id:serial('id').primaryKey().notNull(),
+    createTs: timestamp('createTs').notNull().defaultNow(),
+    value: integer('value').notNull(),
+    description:text('description').notNull(),
+    status: statusEnum('status').notNull()
+})
